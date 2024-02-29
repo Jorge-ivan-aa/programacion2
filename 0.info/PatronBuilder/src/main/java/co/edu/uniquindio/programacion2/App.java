ab012a98c0ba1c141b37ff24f528f246be09e4b2
Spalax-dev <jorgity345@gmail.com>
2024-02-29 15:14:44 -0500

    Se generó el proyecto PatronBuilder

---
+++ 0.info/PatronBuilder/src/main/java/co/edu/uniquindio/programacion2/App.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.programacion2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
