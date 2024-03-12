aca2f65fe4e6636b39dc3794b09807a4264b4214
Spalax-dev <jorgity345@gmail.com>
2024-03-12 09:13:56 -0500

    Se eliminaron DuffBeer y ParcialUno

--- 0.info/DuffBeer/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module co.edu.uniquindio.programacion2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-    exports co.edu.uniquindio.programacion2.controladores;
-    exports co.edu.uniquindio.programacion2;
-    opens co.edu.uniquindio.programacion2.controladores to javafx.fxml;
-
-}
--- 0.info/ParcialUno/src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module co.edu.uniquindio.programacion2 {
-    requires javafx.controls;
-    exports co.edu.uniquindio.programacion2;
-}
